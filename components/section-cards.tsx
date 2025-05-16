39cea276142b2654853aa880f7ac9dde3ab98223
abod <abodesalm45@gmail.com>
2025-05-16 17:35:55 +0300

    cards coloring

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -11,35 +11,41 @@ import {
 
 const data = {
   active_users: 74,
-  total_profit: 267.5,
+  profit: { value: 235.67, up: true, percent: 22.5 },
   top_countries: ["KSA", "Egypt", "Syria", "Turkey", "USA", "Qatar"],
 };
 
 export function SectionCards() {
   return (
-    <div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 grid grid-cols-4 md:grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card lg:px-6">
-      <Card className="@container/card">
+    <div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 grid grid-cols-5 md:grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card lg:px-6">
+      <Card className="@container/card h-fit">
         <CardHeader className="relative">
           <CardDescription>Total Revenue</CardDescription>
           <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-            {data.total_profit}$
+            {data.profit.value}$
           </CardTitle>
           <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingUpIcon className="size-3" />
-              +12.5%
+            <Badge
+              variant="outline"
+              className={`flex gap-1 rounded-lg text-xs ${
+                data.profit.up ? "text-money" : "text-danger"
+              }`}
+            >
+              {data.profit.up ? (
+                <TrendingUpIcon className="size-3" />
+              ) : (
+                <TrendingDownIcon className="size-3" />
+              )}
+              {data.profit.up ? "+" : "-"}
+              {data.profit.percent}%
             </Badge>
           </div>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <TrendingUpIcon className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
+          <div className="text-muted-foreground">profits of this month</div>
         </CardFooter>
       </Card>
+
       <Card className="@container/card">
         <CardHeader className="relative">
           <CardDescription>New Customers</CardDescription>
